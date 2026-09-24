243d394d66ca4e71c8a6bc606454ec0f2241d9bd
a1553846342-dotcom <a1553846342@gmail.com>
2026-09-24 14:26:23 +0800

    release: v1.1.0 (versionCode 198, versionName 1.1.0) + CHANGELOG

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     applicationId = "com.aistudio.novelreader.kxmpzq"
     minSdk = 24
     targetSdk = 35
-    versionCode = 197
-    versionName = "1.0.7"
+    versionCode = 198
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
